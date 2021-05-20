4a2b21dfda62f0238bfe97652a603618c0d86eb4
Александр Могильный <blagimi@yandex.ru>
2021-05-20 16:57:55 +0500

    Chapter 2.1

---
+++ InstanceVSStaticMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;                       // Подлкючение простраства имён
+
+namespace InstanceVSStaticMembers   // Объявление пространства имён
+{
+    class Program                   // Объявление класса Program
+    {
+        static void Main()          // Объявление метода Main
+        {
+            Panda panda1 = new Panda("Pan Dee");    // Создание экземпляра класса Panda и присваивание значение name 
+            Panda panda2 = new Panda("Pan Dah");    
+            Console.WriteLine($" Первую панду зовут: {panda1.Name}\n Вторую панду зовут: {panda2.Name}\n Всего панд: {Panda.Population}");  // Вывод на экран
+            Console.ReadKey();
+        }                           // Конец объявления метода Main
+    }                               // Конец объявления класса Program
+
+    public class Panda              // Объявление класса Panda
+    {
+        public string Name;             // Поле экземпляра класса
+        public static int Population;   // Статическое поле, имеет дело с самим классом
+        public Panda (string name) { Name = name; Population++; }   // Присваиваем значение полю экземпляра, инкрементируем значение статического поля
+    }                               // Конец объявления класса Panda
+}                                   // Конец объявления пространства имён
---
+++ UnitConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;                                       // Подключение пространства имён
+
+namespace UnitConverter                             // Объявление пространсва имён
+{
+    class Program                                   // Объявление класса  Program
+    {
+        static void Main()                          // Объявление метода Main
+        {
+            /* Блок операторов */
+            UnitConverter feetToInchesConverter = new UnitConverter(12);    // Вызов конструктора и создание нового экземпляра для конвертации едениц и присваивание ему значения 12
+            UnitConverter milesToFeeetConverter = new UnitConverter(5280);  // 5280
+
+            Console.WriteLine(feetToInchesConverter.Convert(30));   // 30 * 12 = 360
+            Console.WriteLine(feetToInchesConverter.Convert(100));  // 30 * 12 = 1200
+            Console.WriteLine(feetToInchesConverter.Convert(milesToFeeetConverter.Convert(1))); // (milestofeet(1))5280 * (feettoinches) 12 = 63360
+            /* Конец блока операторов */
+        }                                           // Конец объявления метода Main
+    }                                               // Конец объявление класса Program
+    public class UnitConverter                      // Объявление класса UnitConverter
+    {
+        int ratio;  // Поле для храненения значения
+        public UnitConverter (int unitRatio) { ratio = unitRatio; }     // Конструктор принимающий значение коэфициента и присваювающий это значение полю ratio
+        public int Convert(int unit) { return unit * ratio; }           // Метод принимающий введенное значение и умножающий это значение на значение переменной ratio
+    }                                               // Конец объявления класса UnitConverter
+}                                                   // Конец объявления пространства имён
